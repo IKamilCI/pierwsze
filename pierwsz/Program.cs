09ae794fa7e5d1de19a59ff703ac591e1c49ac3f
wsb-user <oyq37636@zzrgg.com>
2019-11-24 10:23:58 +0100

    Adding numbers works now

--- pierwsz/Program.cs
+++ pierwsz/Program.cs
@@ -18,9 +18,14 @@ namespace pierwsz
                 Console.WriteLine("4. /");
                 Console.WriteLine("5. Exit");
                 int menuOption = Convert.ToInt32(Console.ReadLine());
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
                 if (menuOption == 5)
                     break;
-                int a = Convert.ToInt32(Console.ReadLine());
+                if(menuOption ==1)
+                {
+                    Console.WriteLine(a+b)
+                }
             }
         }
     }
