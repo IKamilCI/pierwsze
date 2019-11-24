fb880189ea5d2eb25f74d5e70ac020118e67c46f
|KamilC| <kamil2523@gmail.com>
2019-11-24 09:21:57 +0100

    Naprawiono 5. Exit

--- pierwsz/Program.cs
+++ pierwsz/Program.cs
@@ -12,12 +12,14 @@ namespace pierwsz
         {
             while(true)
             {
-                int menuOption = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("1. Dodawanie");
-                Console.WriteLine("4. /");
-                Console.WriteLine("5. Exit");
                 Console.WriteLine("2. Odejmowanie");
                 Console.WriteLine("3. *");
+                Console.WriteLine("4. /");
+                Console.WriteLine("5. Exit");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 5)
+                    break;
                 int a = Convert.ToInt32(Console.ReadLine());
             }
         }
